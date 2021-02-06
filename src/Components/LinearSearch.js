b27c9414194ce8a009d2e5fdb382b306c64d888a
Amogh Kulkarni <amogh.kulkarni@cerner.com>
2021-02-06 08:03:55 +0530

    Linear search

--- src/Components/LinearSearch.js
+++ src/Components/LinearSearch.js
@@ -1,4 +1,3 @@
-import { forEach } from 'async';
 import React, { Component } from 'react'
 
 export default class LinearSearch extends Component {
@@ -15,13 +14,27 @@ export default class LinearSearch extends Component {
 
 let a = function linearSearch() {
     let array = [1, 2, 3, 4, 5];
-    console.log(array);
-    let elementToFind = 4;
-
-
-
-
-
-
-
+    let elementToFind = 5;
+    console.log(iterateThroughArray(array, elementToFind));
 }
+
+
+/**
+ * In Linear search time complixity is O(n)
+ * @param {*} array 
+ * @param {*} elementToFind 
+ */
+function iterateThroughArray(array, elementToFind) {
+    let i = 0;
+    for (i = 0; i < array.length; i++) {
+        if (array[i] === elementToFind) {
+            return i;
+        }
+    }
+    return -1
+}
+
+// Improve Linear Search Worst-Case Complexity
+
+// if element Found at last  O(n) to O(1)
+// if element Not found O(n) to O(n/2)
