702ce4836e3450f02fc35cc9818f33a0fd8254db
Amogh Kulkarni <amogh.kulkarni@cerner.com>
2021-02-06 11:33:02 +0530

    binary search

--- src/Components/BinarySearch.js
+++ src/Components/BinarySearch.js
@@ -11,44 +11,67 @@ export default class BinarySearch extends Component {
     }
 }
 
-
+/**
+ * Time complexity of B Search is O(log n)
+ * 
+ * 
+ * There are two methods we can impliment binary search
+ * 
+ * 1. Iterative method
+ * 
+ * 2. Recursive method
+ */
 let a = function binarySearch() {
-    let array = [1, 2, 3, 4, 5, 6, 7, 8];
-    let elementToFind = 5;
-    console.log(binarySearchGoingOn(array, elementToFind));
+    let array = [1, 2, 3, 4, 5, 6, 7, 8, 1231, 2123123];
+    let elementToFind = 1231;
+    console.log(binarySearchRecursiveMethod(array, elementToFind, 0, array.length - 1));
 
 }
 
-
-function binarySearchGoingOn(array, elementToFind) {
-
+/**
+ * Through iterative method
+ */
+function binarySearchIteratorMethod(array, elementToFind) {
     let left = 0;
     let right = array.length - 1;
-
-
     while (left <= right) {
-        let mid = parseInt(left + (right - 1) / 2);
-        console.log("left=" + left + "right=" + right + "mid=" + mid);
+        let mid = parseInt(left + (right - left) / 2);
 
-        console.log("ArrayMId"+array[mid])
         if (array[mid] === elementToFind) {
             return mid;
         }
 
-        else if (array[mid] > elementToFind) {
-            console.log("im here")
+        else if (array[mid] < elementToFind) {
+            left = mid + 1;
+        }
+        else {
             right = mid - 1;
         }
 
-        else {
-            console.log("yes")
-            left = mid + 1;
-        }
-
-        
     }
     return -1;
+}
 
+/**
+ * Through recursive method method
+ */
+function binarySearchRecursiveMethod(array, elementToFind, left, right) {
+
+    if (right >= left) {
+        let mid = parseInt(left + (right - left) / 2);
+        if (array[mid] === elementToFind) {
+            return mid;
+        }
+
+        if (array[mid] > elementToFind) {
+            return binarySearchRecursiveMethod(array, elementToFind, left, mid - 1);
+        }
+
+        else {
+            return binarySearchRecursiveMethod(array, elementToFind, mid + 1, right);
+        }
+    }
+    return -1;
 }
 
 
